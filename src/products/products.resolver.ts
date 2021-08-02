c195c56c8fdbcab9ae7f608f8d9b5ccd4199f2d9
sofiene-chihi <sofienechihi@insat.u-carthage.tn>
2021-08-02 22:53:07 +0100

    update and delete products

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateProductInput } from './dto/create-product-input';
+import { UpdateProductInput } from './dto/update-product-input';
 import { Product } from './product.entity';
 import { ProductsService } from './products.service';
 
@@ -21,6 +22,16 @@ export class ProductsResolver {
     @Query(returns => Product)
     getProduct(@Args('id', {type: () => Int}) id: number): Promise<Product> {
         return this.productsService.findOne(id);
+    }
+
+    @Mutation(returns => Product)
+    updateProduct(@Args('updateProductInput') updateProductInput: UpdateProductInput): Promise<Product> {
+        return this.productsService.updateProduct(updateProductInput);
+    }
+
+    @Mutation(returns => Product)
+    deleteProduct(@Args('id') id: number): Promise<Product> {
+        return this.productsService.deleteProduct(id);
     }
 
 }
